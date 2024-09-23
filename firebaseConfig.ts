0a20e5ff91ac415d1607bb8cff925d974fc382a2
Aishwarya Gunjal <151380287+Aishwaryagunjal6@users.noreply.github.com>
2024-09-24 01:07:33 +0530

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyASoZABocLhC3H-8jFk18OChmKr58WWX4E",
+  authDomain: "codebin-abe4b.firebaseapp.com",
+  projectId: "codebin-abe4b",
+  storageBucket: "codebin-abe4b.appspot.com",
+  messagingSenderId: "261999743210",
+  appId: "1:261999743210:web:01f874ae740db58bdc0793"
+};
